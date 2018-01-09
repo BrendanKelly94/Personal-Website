94aacdcf07ddb984912a7dd10618f96c2865162c
BrendanKelly94 <brendan.kelly94@gmail.com>
2018-01-08 21:22:05 -0800

    added routes

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
+var about = require('./routes/about');
+var contact = require('./routes/contact');
 var users = require('./routes/users');
 
 var app = express();
@@ -24,8 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/about', about);
-app.use('/github', github);
-app.use('/resume', resume);
+app.use('/contact', contact);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
